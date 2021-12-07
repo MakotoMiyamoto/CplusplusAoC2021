a34e356464d06182e84ac4fac69e4452ec3f7b3c
MakotoMiyamoto <makotonomeru@gmail.com>
2021-12-07 11:12:35 -0600

    do not look at the previous version please

--- day7/p1.cpp
+++ day7/p1.cpp
@@ -11,27 +11,6 @@ T sum_diff(It begin, It end, const T &u) {
     return t;
 }
 
-namespace util {
-    template <typename It, typename Pred>
-    It find(It begin, It end, Pred pred) {
-        It it = begin;
-        for (It i = begin + 1; i != end; ++i) {
-            if (pred(it, i)) it = i;
-        }
-        return it;
-    }
-
-    template <typename It>
-    It min(It begin, It end) {
-        return util::find(begin, end, [](It min, It i){ return *min > *i; });
-    }
-
-    template <typename It>
-    It max(It begin, It end) {
-        return util::find(begin, end, [](It max, It i){ return *max < *i; });
-    }
-}
-
 int main() {
     std::vector<int> vec;
 
@@ -41,11 +20,12 @@ int main() {
         vec.push_back(i);
     }
 
-    int min = *util::min(vec.cbegin(), vec.cend()), max = *util::max(vec.cbegin(), vec.cend());
+    int min = *std::min_element(vec.cbegin(), vec.cend()),
+        max = *std::max_element(vec.cbegin(), vec.cend());
     std::vector<int> sums;
     for (int i = min; i < max; ++i) {
         sums.push_back(sum_diff(vec.cbegin(), vec.cend(), i));
     }
     
-    std::cout << *util::min(sums.cbegin(), sums.cend()) << '\n';
+    std::cout << *std::min_element(sums.cbegin(), sums.cend()) << '\n';
 }
